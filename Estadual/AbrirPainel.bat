d4706a436461e9c562408cf146ae11b61db51888
rafaborth <83786459+rafaborth@users.noreply.github.com>
2026-09-10 17:37:17 -0300

    Check for virtual environment before starting script

--- Estadual/AbrirPainel.bat
+++ Estadual/AbrirPainel.bat
@@ -1,2 +1,8 @@
 @echo off
-start "" pythonw "%~dp0painel.py"
+if not exist ".guia_venv\Scripts\pythonw.exe" (
+    echo ERRO: Ambiente nao encontrado.
+    echo Rode "Instalar.bat" primeiro.
+    pause
+    exit /b 1
+)
+start "" ".guia_venv\Scripts\pythonw.exe" "%~dp0painel.py"
